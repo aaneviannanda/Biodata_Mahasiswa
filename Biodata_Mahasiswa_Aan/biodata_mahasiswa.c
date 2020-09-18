de7b545936811b0efcbd8fd0cc8db1ce080f1145
aaneviannanda <aan.eviananda74@gmail.com>
2020-09-18 20:41:39 +0700

    pertama commit

---
+++ Biodata_Mahasiswa_Aan/biodata_mahasiswa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//Program untuk menampilkan Biodata Mahasiswa
+int main(){
+	printf("\t BIODATA MAHASISWA \n\n");
+	printf("Nama      : Aan Evian Nanda\n");
+	printf("NPM       : 19081010040\n");
+	printf("TTL       : Magetan, 31 Desember 2000\n");
+	printf("Alamat    : Ds. Jabung Rt. 06 Rw. 02 Kec. Panekan Kab. Magetan\n");
+	printf("Hobi      : Bermain Game\n");
+	printf("Cita-Cita : Backend developer\n");
+	printf("Status    : Mahasiswa UPN Veteran Jawa Timur\n");
+	printf("Moto Hidup: Semangat dan pantang menyerah");
+	
+return 0;
+}
